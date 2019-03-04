d867b519cc7e6cfa01420bb4db7f6736c8d6dbfb
Joe McColl <joe4kool@gmail.com>
2019-03-04 20:50:39 +0000

    updated app.js with filmbox, export / import working in browser

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FilmBox extends Component{
+
+render(){
+    return(
+        <h1>hello, Im the filmbox!</h1>
+    )
+}
+
+}
+
+export default FilmBox;
